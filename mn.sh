a356527a7213252965d7c59075f7f72f637e3bf0
nicolas-mininet-bolsa <nicolaskagami@gmail.com>
2016-05-24 11:57:58 -0700

    Shortest Path

--- mn.sh
+++ mn.sh
@@ -1,3 +1,3 @@
 #mn --topo single,3 --mac --switch ovsk,protocols=OpenFlow13 --controller remote
-mn --custom topology.py --topo TFTopo --mac --switch ovsk,protocols=OpenFlow13 --controller remote
+mn --custom Mininet/topology.py --topo TFTopo --mac --switch ovsk,protocols=OpenFlow13 --controller remote
 #mn --custom topology.py --topo TFTopo --mac --switch ovsk,protocols=OpenFlow13
